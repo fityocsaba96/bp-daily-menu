3572aa21cca213dbcf77257351650c91725b37e0
Csaba Fityo <csaba.fityo@emarsys.com>
2018-09-20 19:31:28 +0200

    alter restaurant table, add table_name column

--- db/migrations/20180920160617_create_restaurant_table.php
+++ db/migrations/20180920160617_create_restaurant_table.php
@@ -2,10 +2,11 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class CreateRestaurantsTable extends AbstractMigration {
+class CreateRestaurantTable extends AbstractMigration {
 
     public function change(): void {
         $this->table('restaurant')
+             ->addColumn('table_name', 'string', ['limit' => 50])
              ->addColumn('name', 'string', ['limit' => 50])
              ->addColumn('menu_url', 'text')
              ->addColumn('map_url', 'text')

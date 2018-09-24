1117a5921b95f8b967dc0788a322b05b4253a7a1
Csaba Fityo <csaba.fityo@emarsys.com>
2018-09-24 11:17:32 +0200

    setup twig

--- src/AppBuilder.php
+++ src/AppBuilder.php
@@ -2,12 +2,38 @@
 
 namespace BpDailyMenu;
 
+use Psr\Container\ContainerInterface;
 use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
 
 class AppBuilder {
 
+    /**
+     * @var App
+     */
+    private $app;
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
     public function __invoke(): App {
         (new EnvLoader)();
-        return new App();
+        $this->app = new App();
+        $this->container = $this->app->getContainer();
+        $this->setupTwig();
+
+        return $this->app;
+    }
+
+    private function setupTwig(): void {
+        $this->container[Twig::class] = function (ContainerInterface $container) {
+            $view = new Twig(__DIR__ . '/../templates');
+            $basePath = rtrim(str_ireplace(__DIR__ . '/index.php', '',
+                $container->get('request')->getUri()->getBasePath()), '/');
+            $view->addExtension(new TwigExtension($container->get('router'), $basePath));
+            return $view;
+        };
     }
 }

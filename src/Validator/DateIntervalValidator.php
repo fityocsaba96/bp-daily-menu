c2dfb76f11ad533538afdc610f58e2131d0ec057
Csaba Fityo <csaba.fityo@emarsys.com>
2018-10-01 17:19:37 +0200

    DateIntervalValidator: validate from and to date

--- src/Validator/DateIntervalValidator.php
+++ src/Validator/DateIntervalValidator.php
@@ -2,9 +2,22 @@
 
 namespace BpDailyMenu\Validator;
 
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator;
+
 class DateIntervalValidator {
 
-    public function __invoke(string $from, string $to): array {
-        return [];
+    public function __invoke(string $from, string $to): ?string {
+        try {
+            $this->assertDate($from);
+            $this->assertDate($to);
+        } catch (NestedValidationException $exception) {
+            return $exception->getFullMessage();
+        }
+        return null;
+    }
+
+    private function assertDate(string $date): void {
+        Validator::stringType()->date('Y-m-d')->assert($date);
     }
 }
--- tests/Validator/DateIntervalValidatorTest.php
+++ tests/Validator/DateIntervalValidatorTest.php
@@ -11,7 +11,23 @@ class DateIntervalValidatorTest extends TestCase {
      * @test
      */
     public function invoke_givenValidInterval_returnsEmptyArray() {
-        $errors = (new DateIntervalValidator)('2018-10-01', '2018-10-03');
-        $this->assertEquals([], $errors);
+        $error = (new DateIntervalValidator)('2018-10-01', '2018-10-03');
+        $this->assertNull($error);
+    }
+
+    /**
+     * @test
+     */
+    public function invoke_givenInvalidFromDate_containsError() {
+        $error = (new DateIntervalValidator)('2018-1001', '2018-10-03');
+        $this->assertNotNull($error);
+    }
+
+    /**
+     * @test
+     */
+    public function invoke_givenInvalidToDate_containsError() {
+        $error = (new DateIntervalValidator)('2018-10-01', 'fdg');
+        $this->assertNotNull($error);
     }
 }
